edff7cee4f7f6e53171b596086f448f61153dd7d
yancymin <senseslofter@gmail.com>
2018-08-08 19:22:59 +0800

    1

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import "../components/Header.css"
+
+const Header = ({ siteTitle }) => (
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/dowloads">Dowloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
+    </div>
+  </div>
+)
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to <br />design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <Link to="/video">Watch the video</Link>
+      </div>
+    </div>
+  </div>
+)
+
+export default IndexPage
